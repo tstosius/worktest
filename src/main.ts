efbe6374ac881b7ddec8dac7d059fceb78f7f010
Tobias Stosius <tobias.stosius@deutschebahn.com>
2022-08-03 14:56:42 +0200

    update map

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-/// <reference types="@workadventure/iframe-api-typings" />
+/// <reference types='@workadventure/iframe-api-typings' />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-import { ButtonDescriptor } from "@workadventure/iframe-api-typings/Api/iframe/Ui/ButtonDescriptor";
-import {Popup} from "@workadventure/iframe-api-typings/Api/iframe/Ui/Popup"
+import { UIWebsite } from '@workadventure/iframe-api-typings/Api/Events/ui/UIWebsite';
+import { ButtonDescriptor } from '@workadventure/iframe-api-typings/Api/iframe/Ui/ButtonDescriptor';
+import { Popup } from '@workadventure/iframe-api-typings/Api/iframe/Ui/Popup';
+import { bootstrapExtra } from '@workadventure/scripting-api-extra';
 console.log('Script started successfully');
 
 // let currentPopup: any = undefined;
-const  buttons: ButtonDescriptor[] = [
+const buttons: ButtonDescriptor[] = [
     {
         label: 'Reset',
         className: 'error',
@@ -14,42 +15,54 @@ const  buttons: ButtonDescriptor[] = [
     },
 ]
 
+
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ', WA.player.tags)
+
+
+    let website: UIWebsite;
+    WA.room.onEnterLayer('infoPopup').subscribe(() => {
+        openInfoWebsite().then(_website => website = _website)
+    })
+
+    WA.room.onLeaveLayer('infoPopup').subscribe(() => {
+        website.visible = false
+    })
 
     WA.room.onEnterLayer('votePos').subscribe(() => {
-        console.log("VotePos: ", WA.state.votePos);
-		(WA.state.votePos as number) ++;
-	})
-	WA.room.onLeaveLayer('votePos').subscribe(()  => {
-        console.log("VotePos: ", WA.state.votePos);
-        if(WA.state.votePos as number === 0) return ;
-		(WA.state.votePos as number) --;
-	})
-	WA.room.onEnterLayer('voteNeg').subscribe(()  => {
-        console.log("voteNeg: ", WA.state.voteNeg);
-		(WA.state.voteNeg as number) ++;
-	})
-	WA.room.onLeaveLayer('voteNeg').subscribe(() => {
-        console.log("voteNeg: ", WA.state.voteNeg);
-        if(WA.state.voteNeg as number === 0) return ;
-		(WA.state.voteNeg as number) --;
-	})
-	WA.room.onEnterLayer('voteNeut').subscribe(() => {
-        console.log("voteNeut: ", WA.state.voteNeut);
-		(WA.state.voteNeut as number) ++;
-	})
-	WA.room.onLeaveLayer('voteNeut').subscribe(() => {
-        console.log("voteNeut: ", WA.state.voteNeut);
-        if((WA.state.voteNeut as number) === 0) return ;
-		(WA.state.voteNeut as number) --;
-	})
+        console.log('VotePos: ', WA.state.votePos);
+        (WA.state.votePos as number)++;
+    })
+    WA.room.onLeaveLayer('votePos').subscribe(() => {
+        console.log('VotePos: ', WA.state.votePos);
+        if (WA.state.votePos as number === 0) return;
+        (WA.state.votePos as number)--;
+    })
+    WA.room.onEnterLayer('voteNeg').subscribe(() => {
+        console.log('voteNeg: ', WA.state.voteNeg);
+        (WA.state.voteNeg as number)++;
+    })
+    WA.room.onLeaveLayer('voteNeg').subscribe(() => {
+        console.log('voteNeg: ', WA.state.voteNeg);
+        if (WA.state.voteNeg as number === 0) return;
+        (WA.state.voteNeg as number)--;
+    })
+    WA.room.onEnterLayer('voteNeut').subscribe(() => {
+        console.log('voteNeut: ', WA.state.voteNeut);
+        (WA.state.voteNeut as number)++;
+    })
+    WA.room.onLeaveLayer('voteNeut').subscribe(() => {
+        console.log('voteNeut: ', WA.state.voteNeut);
+        if ((WA.state.voteNeut as number) === 0) return;
+        (WA.state.voteNeut as number)--;
+    })
 
     let voteResetPopup: Popup;
     WA.room.onEnterLayer('voteReset').subscribe(() => {
-        voteResetPopup = WA.ui.openPopup("resetPopup","Soll das Voting zurückgesetzt werden?", buttons);
+        voteResetPopup = WA.ui.openPopup('resetPopup', 'Soll das Voting zurückgesetzt werden?', buttons);
     })
     WA.room.onLeaveLayer('voteReset').subscribe(() => {
         voteResetPopup.close();
@@ -62,4 +75,18 @@ WA.onInit().then(() => {
 }).catch(e => console.error(e));
 
 
-export {};
+async function openInfoWebsite(): Promise<UIWebsite> {
+    return await WA.ui.website.open({
+        url: 'http://localhost:3000/info.html',
+        position: {
+            vertical: 'middle',
+            horizontal: 'middle'
+        },
+        size: {
+            width: '50vh',
+            height: '50vh'
+        }
+    })
+}
+
+export { };
